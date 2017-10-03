ac6c84d5e5ed55f8e79d83f2ac4b3e3d45b8d422
iagafonov <iagafonov@seopult.ru>
2017-10-03 16:39:48 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var paths = {
+    scripts: ['./bower_components/ya-map/ya-map-2.1.min.js', './src/**/*.js'],
+    styles: './src/**/*.{scss,sass}',
+    index: './tpl/index.html',
+    testJs: './tpl/geoTargetingTest.js',
+    testCss: './tpl/template.scss',
+    docs: './docs',
+    dist: './dist'
+};
+
+function build() {
+    console.log('build start');
+
+    console.log('build doc');
+
+    var bowerFiles = require('main-bower-files');
+    var embedTemplates = require('gulp-angular-embed-templates');
+    var sourceMaps      = require('gulp-sourcemaps');
+    var cleanCss        = require('gulp-clean-css');
+    var sass            = require('gulp-sass');
+
+    var angularJs = gulp.src(bowerFiles())
+        .pipe(gulp.dest(paths.docs + '/bower_components'));
+
+    var testJs = gulp.src(paths.testJs)
+        .pipe(gulp.dest(paths.docs + '/js'));
+
+    var timeTargetingJs = gulp.src(paths.scripts)
+        .pipe(embedTemplates())
+        .pipe(gulp.dest(paths.docs + '/js'));
+
+    var css = gulp.src([paths.styles, paths.testCss])
+        .pipe(sourceMaps.init())
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(plugins.concat('styles.css'))
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(sourceMaps.write('./'))
+
+        .pipe(gulp.dest(paths.docs + '/css'));
+
+    var version = Date.now();
+
+    gulp.src(paths.index)
+        .pipe(gulp.dest(paths.docs))
+        .pipe(plugins.inject(angularJs, {relative: true, name: 'bower'}))
+        .pipe(plugins.inject(testJs, {relative: true, name: 'test', addSuffix: '?v=' + version}))
+        .pipe(plugins.inject(timeTargetingJs, {relative: true, name: 'component', addSuffix: '?v=' + version}))
+        .pipe(plugins.inject(css, {relative: true, addSuffix: '?v=' + version}))
+        .pipe(gulp.dest(paths.docs));
+
+    console.log('build dist');
+
+    gulp.src(paths.scripts)
+        .pipe(embedTemplates())
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('./dist'));
+
+    gulp.src(paths.styles)
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist'));
+
+    console.log('build end');
+}
+
+function test() {
+    console.log('test');
+}
+
+gulp.task('build', build());
+
+gulp.task('test', test());
---
+++ src/geoTargetingComponent.js
@@ -0,0 +1,75 @@
+(function () {
+    'use strict';
+    angular.module('geoTargetingModule', ['yaMap'])
+        .component('geoTargetingComponent', {
+            bindings: {
+                model: '=',
+                options: '<'
+            },
+            templateUrl: 'geoTargetingComponent.html',
+            controller: [geoTargetingController]
+        });
+
+    function geoTargetingController() {
+        var vm = this;
+
+        vm.geoModel = {};
+
+        vm.getCircle = function(taget) {
+
+        };
+
+        vm.getMap = function(map) {
+
+        };
+
+        vm.getCircle = function (target) {
+            vm.circle = target;
+            vm.circle.events.add('dragend', function (e) {
+                vm.tryFindAddrByCoords(vm.circle.geometry.getCoordinates());
+            })
+        };
+
+        vm.getMap = function (target) {
+            vm.map = target;
+            vm.map.events.add('click', function (e) {
+                var coords = e.get('coords');
+                vm.circle.geometry.setCoordinates(coords);
+                vm.tryFindAddrByCoords(coords);
+            });
+            if (vm.existsAddress) {
+                vm.tryFindByAddr(vm.existsAddress);
+            }
+        };
+
+        vm.tryFindByAddr = function (address) {
+            ymaps.geocode(address, {
+                results: 1
+            }).then(function (res) {
+                if (res) {
+                    var firstGeoObject = res.geoObjects.get(0),
+                        coords = firstGeoObject.geometry.getCoordinates(),
+                        bounds = firstGeoObject.properties.get('boundedBy');
+                    if (bounds) {
+                        vm.circle.geometry.setCoordinates(coords);
+                        vm.map.setCenter(coords);
+                        vm.tryFindAddrByCoords(coords);
+                    }
+                }
+            });
+        };
+
+        vm.tryFindAddrByCoords = function (coords) {
+            ymaps.geocode(coords).then(function (res) {
+                var firstGeoObject = res.geoObjects.get(0);
+                if (firstGeoObject.getAddressLine()) {
+                    $timeout(function () {
+                        vm.currentAddress = firstGeoObject.getAddressLine() ? firstGeoObject.getAddressLine() : 'Не удалось определить адрес.';
+                    }, 0);
+                    vm.circle.geometry.setCoordinates(coords);
+                }
+            });
+        };
+
+    }
+})();
